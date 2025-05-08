64f002385c9131fe59d1e8a1cf347b5888302871
MikailMichael <mdeveden95@gmail.com>
2025-05-08 12:06:50 +0100

    Moved cards to its own component

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react'; 
 import axios from 'axios'; // library for making HTTP requests
-import './App.css'
-import EventCard from './components/EventCard';
 
-function App() {
+function EventCard() {
   const [events, setEvents] = useState([]); // Current list of tech events, function to update, initializes as an empty array
 
   // Runs once when component first loads
@@ -14,11 +12,18 @@ function App() {
   }, []);
 
   return (
-    <div className='p-6'>
-      <h1 className='text-3x1 font-bold mb-4'>London Tech Events</h1>
-        <EventCard /> 
+    <div className='p-6 grid grid-cols-2'>
+      {events.map((event, idx) => (
+        <div key={idx} className='mb-4 p-4 border rounded shadow'>
+          <h2 className='text-xl font-semibold'>{event.title}</h2>
+          <p>{new Date(event.date).toLocaleString()}</p>
+          <p>{event.location}</p>
+          <a href={event.url} target='_blank' className='text-blue-500'>View Event</a>
+          <div className='mt-1 text-sm text-gray-600'>Tags: {event.tags.join(", ")}</div>
+        </div>
+      ))}
     </div>
   )
 }
 
-export default App
+export default EventCard;
