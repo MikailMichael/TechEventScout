bcac50ae362a40051631473c425a5435ff9f3e3d
MikailMichael <mdeveden95@gmail.com>
2025-05-14 14:14:45 +0100

    Added more responsive stylings for grid of cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,7 +51,7 @@ function App() {
         </div>
       </div>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6'>
         {events.length > 0 ? (
           events.map((event, idx) => (
           <EventCard
