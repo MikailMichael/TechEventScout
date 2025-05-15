c904b92e2141d22e5afb9a4cc34fb0b23d54972a
MikailMichael <mdeveden95@gmail.com>
2025-05-15 13:36:12 +0100

    Updated and Improved event card styling, including better representation of event date and time, pills styling for tags, and emotes for links

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -1,11 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
 function EventCard({ title, date, location, url, tags }) {
   return (
-    <div className='bg-white rounded-lg shadow p-4 m-4'>
-        <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
-        <p className="text-gray-600">{date}</p>
-        <p className="text-gray-600">{location}</p>
-        <p className="text-gray-600">{tags}</p>
-        <p className="text-gray=600"><a href={url} target="_blank">Link to event</a></p>
+    <div className='bg-[#2f2f2f] rounded-xl p-6 space-y-4 border border-gray-100 hover:ring-1'>
+        <h2 className="text-xl font-bold text-gray-100 mb-1">{title}</h2>
+        <p className="text-gray-400 text-sm mb-1">
+          {new Date(date).toLocaleString(undefined, {
+            weekday: 'short',
+            month:'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+          })}
+        </p>
+        <p className="text-gray-100 text-sm">{location}</p>
+        <div className="flex flex-wrap gap-2">
+          {tags.split(',').map((tag, i) => (
+            <span key={i} className="bg-blue-800/30 text-gray-100 text-xs font-medium px-3 py-1 rounded-full">{tag.trim()}</span>
+          ))}
+        </div>
+
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center text-blue-400 hover:text-blue-300 text-sm font-medium"
+        >
+          Link to event
+          <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="ml-2 h-4 w-4" />
+        </a>
     </div>
   )
 }
