5f7b1faa2e9244ddc4405da008027c9bd3e8beb3
MikailMichael <mdeveden95@gmail.com>
2025-05-15 14:26:32 +0100

    Created a fetchEvents file

---
+++ backend/fetchEvents.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+const EVENTBRITE_TOKEN = "NK3DIZMBDSG45XWXPDTC";
+/*
+const fetchEventbriteEvents = async () => {
+  try {
+    const response = await axios.get("")
+  }
+}
+*/
