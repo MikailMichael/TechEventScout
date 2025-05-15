eb8e6dd2b245ada23faf8c152f950fcccd065227
MikailMichael <mdeveden95@gmail.com>
2025-05-15 13:34:48 +0100

    Removed redundant styling, updated colors to match app

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ function SearchBar({ onSearch }) {
   return (
     <input
       id="searchInp"
-      className="form-control w-[300px] flex-1 max-w-[400px] py-2 px-2 border border-white-400 rounded-md focus:outline-none focus:ring-2 focus:ring-white-500 hover:ring-1"
+      className="form-control w-[300px] flex-1 max-w-[400px] py-2 px-2 border border-gray rounded-md focus:outline-none focus:ring-2 hover:ring-1"
       placeholder="Search events"
       value={input}
       onChange={handleChange}
