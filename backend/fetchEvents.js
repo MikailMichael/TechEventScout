891d9154ea1ccb4aef3641f0ce05f282f22f0de0
Mikail <128922784+MikailMichael@users.noreply.github.com>
2025-05-16 08:08:59 +0100

    Removed API key visibility

--- backend/fetchEvents.js
+++ backend/fetchEvents.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 
-const EVENTBRITE_TOKEN = "NK3DIZMBDSG45XWXPDTC";
+const EVENTBRITE_TOKEN = "";
 /*
 const fetchEventbriteEvents = async () => {
   try {
