96a7097e5bc84387d81852dce5202068440412ad
MikailMichael <mdeveden95@gmail.com>
2025-05-21 11:57:37 +0100

    Updated the tag colours const to match the tagMaps.json tags, added more varying colours too

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -3,14 +3,22 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 function EventCard({ title, date, time, location, link, tags }) {
 const tagColors = {
-  JavaScript: 'bg-yellow-700 text-yellow-100',
-  React: 'bg-blue-700 text-blue-100',
-  Node: 'bg-green-700 text-green-100',
-  TypeScript: 'bg-indigo-700 text-indigo-100',
-  CSS: 'bg-pink-700 text-pink-100',
-  HTML: 'bg-red-700 text-red-100',
-  Hackathon: 'bg-purple-700 text-purple-100',
-  Networking: 'bg-teal-700 text-teal-100'
+  Tech: 'bg-yellow-700 text-yellow-100',
+  Business: 'bg-blue-700 text-blue-100',
+  STEM: 'bg-green-700 text-green-100',
+  Startups: 'bg-indigo-700 text-indigo-100',
+  Sustainability: 'bg-pink-600 text-pink-100',
+  "Software Development": 'bg-red-600 text-red-100',
+  Diversity: 'bg-purple-700 text-purple-100',
+  Networking: 'bg-teal-600 text-teal-100',
+  Marketing: 'bg-lime-700 text-lime-100',
+  AI: 'bg-amber-700 text-amber-100',
+  Entrepreneurship: 'bg-sky-700 text-sky-100',
+  Careers: 'bg-fuchsia-700 text-fuchsia-100',
+  "Health & MedTech": 'bg-emerald-700 text-emerald-100',
+  "Tech for Kids": 'bg-rose-700 text-rose-100',
+  "Cloud Computing": 'bg-cyan-700 text-cyan-100',
+  "UI/UX": 'bg-gray-700 text-gray-100'
 };
 
   return (
@@ -28,7 +36,7 @@ const tagColors = {
         <p className="text-gray-100 text-sm mb-2">{location}</p>
         <div className="flex flex-wrap gap-2 mb-2">
           {tags.split(',').map((tag, i) => {
-            const colorClass = tagColors[tag.trim()] || 'bg-gray-700 text-gray-200';
+            const colorClass = tagColors[tag.trim()] || 'bg-zinc-700 text-zinc-200';
             return (
               <span key={i} className={`text-sm font-medium px-3 py-1 rounded-full ${colorClass}`}>{tag.trim()}</span>
             );
