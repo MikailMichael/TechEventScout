b34634e82ab783bc562a4afb95002c07110da276
MikailMichael <mdeveden95@gmail.com>
2025-05-21 15:41:06 +0100

    Added a --help flag to fetchEvents.js to describe the script and options available

--- backend/scraper/fetchEvents.js
+++ backend/scraper/fetchEvents.js
@@ -1,14 +1,34 @@
 // fetchEvent.js
 // Main entry point for scraping and storing event data from multiple sources.
 
-const path = require('path');
+const { log, saveJSON } = require('./utils');
 
+// Show usage if --help flag is passed
+if (process.argv.includes('--help')) {
+  log(`
+    Usage: node fetchEvents.js [number_of_pages]
+    
+    Description:
+      Scrapes tech events from Eventbrite and Meetup and saves them to a JSON file.
+
+    Options:
+      [number_of_pages]   Optional. Number of Eventbrite pages to scrape (default: 2).
+      --help              Show this help message.
+    
+    Examples:
+      node fetchEvents.js 3       # Scrape 3 pages
+      node fetchEvents.js         # Scrape 2 pages (default)
+  `);
+  process.exit(0);
+}
+
+const path = require('path');
 const scrapeEventbrite = require('./eventbrite');
 const scrapeMeetup = require('./meetup');
-const { log, saveJSON } = require('./utils');
 
 const EVENTS_FILE = path.join(__dirname, "..", "data", "events.json");
 const NUM_OF_PAGES = parseInt(process.argv[2]) || 2;
+
 
 /**
  * Main function that orchestrates event scraping from all sources,
