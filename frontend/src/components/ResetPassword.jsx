f19ac180dafa1edd2260ccb023261f29b81da21a
MikailMichael <mdeveden95@gmail.com>
2025-06-01 15:04:19 +0100

    Improved styling to match main site, added a return button

--- frontend/src/components/ResetPassword.jsx
+++ frontend/src/components/ResetPassword.jsx
@@ -30,23 +30,29 @@ export default function ResetPassword() {
 
   if (confirmed) {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-neutral-900 text-white">
+      <div className="min-h-screen w-screen flex items-center justify-center bg-neutral-900 x-4 text-white ">
         <p>Your password has been reset. You can now log in.</p>
+        <a href="http://localhost:5173">
+          <button
+            type="button"
+            className="w-full font-bold py-2 px-4 bg-neutral-700 border border-neutral-400 text-white rounded-md hover:ring-1 transition"
+          >Return To Login</button>
+        </a>
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen flex-items-center justify-center bg-neutral-900">
+    <div className="min-h-screen w-screen flex items-center justify-center bg-neutral-900">
       <form onSubmit={handleReset} className="bg-neutral-800 p-6 rounded-md border border-neutral-400 space-y-4 max-w-md w-full text-white">
         <h2 className="text-xl font-bold text-center">Reset Password</h2>
-        <input 
+        <input
           type="password"
           required
           placeholder="New Password"
           value={newPassword}
           onChange={(e) => setNewPassword(e.target.value)}
-          className="w-full px-4- py-2 border border-neutral-400 rounded-md bg-neutral-700 text-white placeholder-neutral-400 focus:outline-none focus:ring-2 focus:ring-gray-100"
+          className="w-full px-4 py-2 border border-neutral-400 rounded-md bg-neutral-700 text-white placeholder-neutral-400 focus:outline-none focus:ring-2 focus:ring-gray-100"
         />
         <button
           type="submit"
