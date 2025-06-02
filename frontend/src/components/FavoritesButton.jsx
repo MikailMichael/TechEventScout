ef1200eb0a5023718dd0dde44e652a8d981661af
MikailMichael <mdeveden95@gmail.com>
2025-06-02 13:59:57 +0100

    Updated component to receive a param to show the FavouritesModal

--- frontend/src/components/FavoritesButton.jsx
+++ frontend/src/components/FavoritesButton.jsx
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-function FavoritesButton({ }) {
+function FavoritesButton({ onClick }) {
   return (
-    <button type="button" className="btn text-gray-100 font-bold py-2 px-4 border border-gray-100 bg-neutral-800 rounded-md focus:outline-none focus:ring-2 hover:ring-1 transition">
+    <button onClick={onClick} type="button" className="btn text-gray-100 font-bold py-2 px-4 border border-gray-100 bg-neutral-800 rounded-md focus:outline-none focus:ring-2 hover:ring-1 transition">
       <FontAwesomeIcon icon={faStar} />
     </button>
   )
