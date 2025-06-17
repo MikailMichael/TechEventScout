697155019bf75e651158a95be71e28342eff0bc3
MikailMichael <mdeveden95@gmail.com>
2025-06-17 17:40:26 +0100

    Added all the new components to Header and tidied it up

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,26 @@
 import logo from '../assets/logo.png';
 import LoginButton from './LoginButton';
+import FavoritesButton from './FavoritesButton';
+import Profile from './Profile';
 
-function Header({ user, onLogOut, onShowAuth }) {
+function Header({ user, onLogOut, onShowAuth, showFavourites }) {
   return (
-    <header className="w-full h-[68px] flex justify-between border-b-2 border-[#181818] px-[58px] py-[16px]">
+    <header className="w-full h-17 flex justify-between border-b-2 border-[#181818] px-14.5 py-4">
       <div className='flex flex-inline items-center gap-1'>
-        <img src={logo} alt="Logo" className="h-[36px] w-auto" />
-        <h1 className='text-[28px] leading-[100%] bg-gradient-to-br from-[#7C82FF] to-[#C355F5] bg-clip-text text-transparent'>London Tech Events</h1>
+        <img src={logo} alt="Logo" className="h-9 w-auto" />
+        <h1 className='text-[28px] bg-gradient-to-br from-[#7C82FF] to-[#C355F5] bg-clip-text text-transparent'>London Tech Events</h1>
       </div>
 
-      <LoginButton user={user} onLogOut={onLogOut} onShowAuth={onShowAuth} />
+      {user ? (
+        <div className='flex flex-inline items-center gap-2'>
+          <FavoritesButton onClick={showFavourites} />
+          <Profile user={user} onLogOut={onLogOut} />
+        </div>
+      ) : (
+        <div className='flex flex-inline items-center gap-1'>
+          <LoginButton onShowAuth={onShowAuth} />
+        </div>
+      )}
     </header>
   );
 }
