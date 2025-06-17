9a9b51c7cff457ce545f07b842984587a660d0ba
MikailMichael <mdeveden95@gmail.com>
2025-06-17 17:35:57 +0100

    Updated sizing to be responsive

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,12 +1,12 @@
 import login from '../assets/login.png';
 
-function LoginButton({ user, onLogOut, onShowAuth }) {
+function LoginButton({ onShowAuth }) {
   return (
         <button
           onClick={onShowAuth}
-          className="text-white text-sm font-semibold py-[6px] px-[8px] gap-[8px] flex flex-inline items-center rounded-md h-[36px] w-[86px] bg-gradient-to-br from-[#7C82FF] to-[#C355F5]"
+          className="text-white text-sm font-semibold py-1.5 px-2 gap-2 flex flex-inline items-center rounded-md h-9 bg-gradient-to-br from-[#7C82FF] to-[#C355F5]"
         >
-          <img src={login} alt="Login icon" className='h-[24px] w-auto' />
+          <img src={login} alt="Login icon" className='h-6 w-auto' />
           Login
         </button>
   );
