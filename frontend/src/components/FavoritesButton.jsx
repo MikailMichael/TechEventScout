53159638fd2b9a90af4fcd0e8eb0ab7dfd462c6a
MikailMichael <mdeveden95@gmail.com>
2025-06-17 18:37:05 +0100

    Remove redundant button styling, rounded buttons more

--- frontend/src/components/FavoritesButton.jsx
+++ frontend/src/components/FavoritesButton.jsx
@@ -2,7 +2,7 @@ import bookmark from "../assets/bookmark.png";
 
 function FavoritesButton({ onClick }) {
   return (
-    <button onClick={onClick} type="button" className="text-white text-sm font-semibold py-1.5 px-2 gap-2 flex flex-inline items-center rounded-md h-9 bg-gradient-to-br from-[#7C82FF] to-[#C355F5]">
+    <button onClick={onClick} type="button" className="text-white text-sm font-semibold py-1.5 px-2 gap-2 flex flex-inline items-center rounded-lg h-9 bg-gradient-to-br from-[#7C82FF] to-[#C355F5]">
       <img src={bookmark} alt="Bookmark icon" className="h-6 w-auto" />
       Bookmarks
     </button>
