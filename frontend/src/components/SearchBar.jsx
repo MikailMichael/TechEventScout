b20f08bb9baeee5ed73018dfb5c51098aacada51
MikailMichael <mdeveden95@gmail.com>
2025-06-19 18:59:14 +0100

    Add icon for searchbar, update search bar styling

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import searchIcon from '../assets/search.png';
 
 function SearchBar({ onSearch }) {
   const [input, setInput] = useState("");
@@ -10,13 +11,18 @@ function SearchBar({ onSearch }) {
   }
 
   return (
-    <input
-      id="searchInp"
-      className="form-control w-[300px] flex-1 max-w-[400px] py-2 px-2 border border-gray bg-neutral-800 placeholder-neutral-400 rounded-md focus:outline-none focus:ring-2 hover:ring-1 transition"
-      placeholder="Search events"
-      value={input}
-      onChange={handleChange}
-    />
+    <div className='relative my-6 px-14.5'>
+      <input
+        id="searchInp"
+        className="form-control pl-12 pr-3 py-3 w-full border border-border-gray bg-background-2 placeholder-neutral-400 rounded-lg focus:outline-none focus:ring-2 hover:ring-1 transition"
+        placeholder="Search events by title, description, location or tags..."
+        value={input}
+        onChange={handleChange}
+      />
+
+      <img src={searchIcon} alt="Search icon" className='pointer-events-none absolute left-17.5 top-1/2 transform -translate-y-1/2 h-6 w-auto' />
+    </div>
+
   )
 }
 
