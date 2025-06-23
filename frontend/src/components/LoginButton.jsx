5170971117f6d53251d980ef7b2a366851f8a302
MikailMichael <mdeveden95@gmail.com>
2025-06-23 11:27:17 +0100

    Add hover stylings to reverse the gradient

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -4,7 +4,7 @@ function LoginButton({ onShowAuth }) {
   return (
         <button
           onClick={onShowAuth}
-          className="text-white text-sm font-semibold py-1.5 px-2 gap-2 flex flex-inline items-center rounded-lg h-9 bg-gradient-to-br from-grad-purp-start to-grad-purp-end"
+          className="text-white text-sm font-semibold py-1.5 px-2 gap-2 flex flex-inline items-center rounded-lg h-9 bg-gradient-to-br from-grad-purp-start to-grad-purp-end hover:from-grad-purp-end hover:to-grad-purp-start"
         >
           <img src={login} alt="Login icon" className='h-6 w-auto' />
           Login
