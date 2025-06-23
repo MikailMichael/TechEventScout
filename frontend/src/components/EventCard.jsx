efb86f68a10f48dc41f82729cc15918b347b40c5
MikailMichael <mdeveden95@gmail.com>
2025-06-23 17:57:47 +0100

    Add date & time icons, reformatted title, description, date & time in event card component

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -1,9 +1,11 @@
 import TagPill from "./TagPill";
 import BookmarkButton from "./BookmarkButton";
+import calender from "../assets/date-transparent.png";
+import clock from "../assets/clock.png";
 
 function EventCard({ id, title, img, description, date, time, location, link, tags, onFavourite, isFavourited }) {
   return (
-    <div className='flex bg-neutral-800 rounded-xl overflow-hidden border border-gray-700 hover:ring-1 transition'>
+    <div className='flex bg-background-2 rounded-lg'>
       {/* Image */}
       {img && (
         <img
@@ -14,11 +16,25 @@ function EventCard({ id, title, img, description, date, time, location, link, ta
       )}
 
       {/* Content */}
-
-      <div className="flex-1 p-4 flex flex-col justify-between">
-        <div>
-          <h2 className="text-xl font-bold text-white">{title}</h2>
-          <p className="text-gray-400 text-sm mt-1">
+      <div className="flex-1 py-4 px-5 flex flex-col justify-between">
+        <div className="space-y-2">
+          <h2 className="text-white text-left">{title}</h2>
+          <p className="text-neutral-400 text-sm line-clamp-2 text-left ">{description}</p>
+          <div className="flex space-x-16">
+            <div className="flex space-x-1 items-center">
+              <img src={calender} alt="Calender icon" className="h-6 w-auto" />
+              <p className="text-sm text-neutral-400">{new Date(date).toLocaleDateString('en-GB', {
+                weekday:  'short',
+                day:      '2-digit',
+                month:    'short'
+              })}</p>
+            </div>
+            <div className="flex space-x-1 items-center">
+              <img src={clock} alt="Time icon" className="h-6 w-auto" />
+              <p className="text-sm text-neutral-400">{time}</p>
+            </div>
+          </div>
+          <p className="text-neutral-400 text-sm mt-1">
             {new Date(`${date}T${time}`).toLocaleString(undefined, {
               weekday: 'short',
               month: 'short',
@@ -28,7 +44,7 @@ function EventCard({ id, title, img, description, date, time, location, link, ta
             })}{" • "}
             {location}
           </p>
-          <p className="mt-2 text-gray-200 text-sm line-clamp-2">{description}</p>
+
         </div>
 
         <div className="mt-4 flex items-center justify-between">
@@ -43,7 +59,7 @@ function EventCard({ id, title, img, description, date, time, location, link, ta
 
       {/* Favorite Button */}
       <div className="p-2 flex items-start">
-            <BookmarkButton isFavourited={isFavourited} onClick={() => onFavourite(id, title)} />
+        <BookmarkButton isFavourited={isFavourited} onClick={() => onFavourite(id, title)} />
       </div>
     </div>
   )
