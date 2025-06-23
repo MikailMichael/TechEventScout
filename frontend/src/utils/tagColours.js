db9e7df2a7999a48b6842121161829fb17aac02e
MikailMichael <mdeveden95@gmail.com>
2025-06-23 12:44:58 +0100

    Update tag colours to better match website stylings

--- frontend/src/utils/tagColours.js
+++ frontend/src/utils/tagColours.js
@@ -1,21 +1,22 @@
 // tagColours.js
 const tagColours = {
-  Tech: 'bg-yellow-700 text-yellow-100',
-  Business: 'bg-blue-700 text-blue-100',
-  STEM: 'bg-green-700 text-green-100',
-  Startups: 'bg-indigo-700 text-indigo-100',
-  Sustainability: 'bg-pink-600 text-pink-100',
-  "Software Development": 'bg-red-600 text-red-100',
-  Diversity: 'bg-purple-700 text-purple-100',
-  Networking: 'bg-teal-600 text-teal-100',
-  Marketing: 'bg-lime-700 text-lime-100',
-  AI: 'bg-amber-700 text-amber-100',
-  Entrepreneurship: 'bg-sky-700 text-sky-100',
-  Careers: 'bg-fuchsia-700 text-fuchsia-100',
-  "Health & MedTech": 'bg-emerald-700 text-emerald-100',
-  "Tech for Kids": 'bg-rose-700 text-rose-100',
-  "Cloud Computing": 'bg-cyan-700 text-cyan-100',
-  "UI/UX": 'bg-gray-700 text-gray-100'
+  Tech: 'border-neutral-100',
+  Business: 'border-[#0097FF]',
+  STEM: 'border-[#00AF00]',
+  Startups: 'border-[#EC8100]',
+  Sustainability: 'border-[#00C6DB]',
+  "Software Development": 'border-[#bd1e1e]',
+  Diversity: 'border-[#FC4400]',
+  Networking: 'border-[#CE47EB]',
+  Marketing: 'border-[#48A287]',
+  AI: 'border-[#6689FF]',
+  Entrepreneurship: 'border-[#fcaf58]',
+  Careers: 'border-[#b7245c]',
+  "Health & MedTech": 'border-[#800020]',
+  "Tech for Kids": 'border-[#6C3082]',
+  "Cloud Computing": 'border-[#577399]',
+  "UI/UX": 'border-[#9F8170]',
+  Other: 'border-[#8B8589]'
 };
 
 export default tagColours;
