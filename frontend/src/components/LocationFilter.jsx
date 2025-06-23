a10405efaa047e7723687d9925b7827ac8c83c2a
MikailMichael <mdeveden95@gmail.com>
2025-06-23 11:32:28 +0100

    Update locations filter sidebar with hover effects & correct stylings

--- frontend/src/components/LocationFilter.jsx
+++ frontend/src/components/LocationFilter.jsx
@@ -5,13 +5,13 @@ function LocationFilter({ locations, selected, onChange }) {
     <div className="space-y-3 p-4.5 bg-background-2 rounded-lg">
       <div className='flex space-x-2'>
         <img src={filter} alt="Bookmark icon" className="h-6 w-auto" />
-        <h3 className="font-semibold text-left">Locations</h3>
+        <h3 className="text-left">Locations</h3>
       </div>
 
       <ul className="space-y-3">
         <li>
           <button
-            className={`w-full text-left px-2 py-1 rounded-lg ${selected === "" ? "bg-gradient-to-br from-grad-purp-start to-grad-purp-end text-white" : "hover:bg-gray-800"}`}
+            className={`w-full text-sm font-semibold text-left px-2 py-1 rounded-lg ${selected === "" ? "bg-gradient-to-br from-grad-purp-start to-grad-purp-end text-white" : "hover:bg-gradient-to-br hover:from-grad-purp-start-hover hover:to-grad-purp-end-hover"}`}
             onClick={() => onChange("")}
           >
             All Locations</button>
@@ -19,7 +19,7 @@ function LocationFilter({ locations, selected, onChange }) {
         {locations.map((loc) => (
           <li key={loc}>
             <button
-              className={`w-full text-left px-2 py-1 rounded ${selected === loc ? "bg-gradient-to-br from-grad-purp-start to-grad-purp-end text-white" : "hover:bg-gray-800"}`}
+              className={`w-full text-sm font-semibold text-left px-2 py-1 rounded-lg ${selected === loc ? "bg-gradient-to-br from-grad-purp-start to-grad-purp-end text-white" : "hover:bg-gradient-to-br hover:from-grad-purp-start-hover hover:to-grad-purp-end-hover"}`}
               onClick={() => onChange(loc)}
             >{loc}</button>
           </li>
