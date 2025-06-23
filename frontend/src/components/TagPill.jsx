9c0b0f110eabd93eac0dcd21f3766a967bc6235f
MikailMichael <mdeveden95@gmail.com>
2025-06-23 14:30:47 +0100

    Add a tag pill class to match colours but without hover and selected functionality

--- frontend/src/components/TagPill.jsx
+++ frontend/src/components/TagPill.jsx
@@ -1,9 +1,9 @@
 import tagColours from "../utils/tagColours";
 
 function TagPill({ tag }) {
-    const colourClass = tagColours[tag.trim()] || "border-[#8B8589]";
+    const hex = tagColours[tag.trim()] || "#8B8589";
     return (
-        <span className={`text-xs px-2 py-1 rounded-lg border border-2 border-[${colourClass}]`}>{tag.trim()}</span>
+        <span style={{"--tag-colour": hex}} className={`text-xs px-2 py-1 rounded-lg tag-pill`}>{tag.trim()}</span>
     );
 }
 
