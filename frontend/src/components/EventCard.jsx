fc36fe402912e5b781b1d57c7311868bd325239d
MikailMichael <mdeveden95@gmail.com>
2025-08-26 13:19:41 +0100

    Set expire tag height to fixed height, eventcard now flex-col on smaller screens for a more vertical card

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -10,13 +10,13 @@ function EventCard({ id, title, img, description, date, time, location, link, ta
   const isExpired = new Date(`${date}T${time}`) < new Date();
 
   return (
-    <div className='flex bg-background-2 rounded-lg'>
+    <div className='flex flex-col sm:flex-row bg-background-2 rounded-lg'>
       {/* Image */}
       {img && (
         <img
           src={img}
           alt={title}
-          className="w-70 h-70 object-cover flex-shrink-0 rounded-lg"
+          className="w-full sm:w-70 h-70 object-cover flex-shrink-0 rounded-t-lg sm:rounded-l-lg sm:rounded-t-none"
         />
       )}
 
@@ -24,7 +24,7 @@ function EventCard({ id, title, img, description, date, time, location, link, ta
       <div className="flex-1 py-4 px-5 flex flex-col justify-between items-start">
         <div className="flex space-x-2">
           <h2 className="text-white text-left">{title}</h2>
-          {isExpired && (<span className="text-xs font-semibold text-red-100 bg-red-900 px-2 py-1 rounded">Expired</span>)}
+          {isExpired && (<span className="text-xs font-semibold text-red-100 bg-red-900 h-6 px-2 py-1 rounded">Expired</span>)}
         </div>
         <p className="text-neutral-400 text-sm line-clamp-2 text-left ">{description}</p>
 
