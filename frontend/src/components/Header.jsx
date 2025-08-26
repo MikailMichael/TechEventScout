f7a89864b37b9ddabed0bd0cab7373f3b354a3a0
MikailMichael <mdeveden95@gmail.com>
2025-08-26 13:13:12 +0100

    Add responsive desgin to Header, hide text and less padding on phone screens

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,10 +5,10 @@ import Profile from './Profile';
 
 function Header({ user, onLogOut, onShowAuth, showFavourites }) {
   return (
-    <header className="w-full h-17 flex justify-between border-b-2 border-[#181818] px-14.5 py-4">
+    <header className="w-full h-17 flex justify-between border-b-2 border-[#181818] px-4 sm:px-14.5 py-4">
       <div className='flex flex-inline items-center gap-1'>
         <img src={logo} alt="Logo" className="h-9 w-auto" />
-        <h1 className='text-[28px] bg-gradient-to-br from-grad-purp-start to-grad-purp-end bg-clip-text text-transparent'>London Tech Events</h1>
+        <h1 className='text-[28px] bg-gradient-to-br from-grad-purp-start to-grad-purp-end bg-clip-text text-transparent sm:inline hidden'>London Tech Events</h1>
       </div>
 
       {user ? (
