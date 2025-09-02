871115a84010805eb1fb0f893ed1822854de5e74
MikailMichael <mdeveden95@gmail.com>
2025-09-02 13:30:32 +0100

    Add default path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import Auth from './components/Auth';
 import ResetPassword from './components/ResetPassword';
 import Home from './components/Home';
+import { Toaster } from 'react-hot-toast';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      {/* <Route path='/auth' element={<Auth />} /> */}
-      <Route path='/reset-password' element={<ResetPassword />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route path='/auth' element={<Auth />} /> */}
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
   );
 }
+
+//      <Toaster position='top-right' toastOptions={{ className: '', style: {} }}/>
