bf5a26a690a9a2c8bf9a728653697908fb96ab87
MikailMichael <mdeveden95@gmail.com>
2025-09-02 13:34:09 +0100

    Add safelist toast classes to ensure they do not get purged as the elements render dynamically

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx,css}",
     "./public/index.html",
   ],
+  safelist: [
+    'toast-success',
+    'toast-error',
+    'toast-loading',
+  ],
   theme: {
     extend: {
       colors: {
